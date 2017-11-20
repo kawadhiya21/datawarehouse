bea93d4c1f56ac3e508832b0cfcc86a8bd32dfd8
Kshitij Awadhiya <kawadhiya21@gmail.com>
2017-11-21 04:24:10 +0530

    fix config

--- listener/config/config.js
+++ listener/config/config.js
@@ -1,12 +1,12 @@
 YAML = require('yamljs');
 
 const path = require('path');
-const file_path = path.dirname(main.require.filename);
+const file_path = path.dirname(require.main.filename);
 
-let allowed_events = YAML.load(file_path+'allowed_events.yml');
-let config = YAML.load(file_path+'config.yml');
+const allowed_events = YAML.load(file_path+'/config/allowed_events.yml');
+const config = YAML.load(file_path+'/config/config.yml');
 
 module.exports = {
-  allowed_events: allowed_events,
-  config: config
+  'allowed_events' : allowed_events,
+  'config' : config
 }
